685e905053ebe825752f087c6a21996d7250e414
Yoneji Iori <ioriveur@coins.tsukuba.ac.jp>
2012-10-24 06:11:11 +0900

    generic module defined

--- tree.hs
+++ tree.hs
@@ -2,45 +2,79 @@ module Main where
 
 newtype State = State (Register, [State])
               deriving (Eq, Show)
-data Register = I
-              | R [Int]
+data Register = Init
+              | Valid [Int]
 							deriving (Eq, Show)
 type Input    = [Int]
 type Output   = [Int]
 
 --unState :: State -> (Signal, [State])
 --unState (State(sig,[st])) = (sig,[st])
---
+
 --submodstate :: State -> [State]
 --submodstate = snd . unState
 
---genericmodule :: Modparam -> (Input, State) -> (Output, State)
---genericmodule prm
+type Module = (Input, State) -> (Output, State)
+
+data Modparam =
+	Modparam {
+		 output  :: ((Input,[Int],[Output]) -> Output)
+		,statev' :: ((Input,[Int],[Output]) -> Register)
+		,stinitv :: [Int]
+		,submod  :: [(Input, State) -> (Output, State)]
+		,assign  :: [(Input -> [Int])]
+		}
+
+genericmodule :: Modparam -> (Input, State) -> (Output, State)
+genericmodule p (ipv, State( regv, substate ))
+    | regv == Init = (out, State(Valid $ load Init, substate'))
+		| otherwise    = (out, State(ns, substate'))
+		where
+		out = (output p) (ipv,reg,assign')
+		ns  = (statev' p) (ipv,reg,assign')
+		sbm = mas (submod p) (assign p) ipv substate
+		reg = load regv
+		assign' = map fst sbm
+		substate' = map snd sbm
+		mas :: [((Input, State) -> (Output, State))] -> [(Input -> [Int])] -> Input -> [State] -> [(Output, State)]
+		mas [] _ _ _ = []
+		mas m a iv s = ((head m) ((head a) iv, head s)):(mas (tail m) (tail a) iv (tail s))
+		load :: Register -> [Int]
+		load Init        = stinitv p--initial vector
+		load (Valid reg) = reg
 
 main :: IO()
-main = print $ test ([0,1], State(I,[State(I,[])])) 10
+main = print $ test ([0], State(Init,[])) 100
 
 test :: (Input, State) -> Int -> [Output]
 test mdl k
 	| k == 0 = [oup]
-	| otherwise = oup:(test (([oup0+1,oup1]), st') (k-1))
+	| otherwise = oup:(test (([oup0+1]), st') (k-1))
    where
-	   (oup@(oup0:oup1:_),st') = mod1 mdl
+	   (oup@(oup0:_),st') = mod1 mdl
 
 mod1 :: (Input, State) -> (Output, State)
-mod1 ((ip:ips), State(ds, mo@(moc:[])))
-    | ds == I = (0:out1, State(R[1],[st1]))
-		| otherwise = (out, State(R[ip], [st1]))
-		where
-		(out1, st1) = mod2 (ips, moc)
-		out0 = load ds
-		out = (out0++out1)
-		load r@(R ip) = ip
-
-mod2 :: (Input, State) -> (Output, State)
-mod2 ((ip:ips), State(ds, []))
-    | ds == I = ([0], State(R[0],[]))
-		| otherwise = ([1 + load ds], State(R[1 + load ds], []))
-		where
-		load r@(R (s:[])) = s
-
+mod1 = genericmodule p
+  where
+    p = Modparam out ns [0] [] []
+    out (_,r,_) = r
+    ns  (i,_,_) = Valid i
+-- mod1 :: (Input, State) -> (Output, State)
+-- mod1 ((ip:ips), State( dat, substate ))
+--     | dat == I   = (out, State(R $ load dat,[st1]))
+-- 		| otherwise = (out, State(R[ip], [st1]))
+-- 		where
+-- 		(out1, st1) = mod2 (ips, head substate)
+-- 		out0 = load dat
+-- 		out = (out0++out1)
+-- 		load :: Register -> [Int]
+-- 		load I        = [1] --initial vector
+-- 		load r@(R reg) = reg
+-- 
+-- mod2 :: (Input, State) -> (Output, State)
+-- mod2 ((ip:ips), State(ds, []))
+--     | ds == I = ([0], State(R[0],[]))
+-- 		| otherwise = ([1 + load ds], State(R[1 + load ds], []))
+-- 		where
+-- 		load r@(R (s:[])) = s
+-- 
